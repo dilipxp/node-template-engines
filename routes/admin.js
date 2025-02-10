81918c68cc5e948c7ffa0053a259e00d554ef61e
Dilip Singh Kushwah <dkhushwah@ingenuitygaming.com>
2025-02-10 22:41:44 +0530

    using handlebars layout

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', {pageTitle:'Add Prodcut', path:'/admin/add-product'})
+  res.render("add-product", { pageTitle: "Add Prodcut", path: "/admin/add-product", activeAddProduct: true, productCss: true, formsCss: true });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   // console.log(req.body);
-  products.push({title: req.body.title})
-  res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 // module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
   // console.log(adminData.products)
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products
-  res.render('shop',{prods: products, pageTitle:'Shop', path:'/Shop', hasProducts: products.length > 0});
+  res.render('shop',{prods: products, pageTitle:'Shop', path:'/Shop', hasProducts: products.length > 0, activeShop:true, productCss: true});
 });
 
 module.exports = router;
